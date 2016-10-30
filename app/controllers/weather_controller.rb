d603e9474be7ead7236b25350a15ad903a4acc15
Will Farrell <email": null>
2016-10-30 16:13:52 -0500

    Adjust create path in weather to work on regular html

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -2,26 +2,15 @@ class WeatherController < ApplicationController
   require 'open_weather'
 
   def create
-    p "In weather controller"
     weather = Weather.new(weather_params)
     options = { APPID: ENV['OPEN_WEATHER_TOKEN']}
     @conditions = OpenWeather::Current.city("#{weather.city}, #{weather.state}", options)
-    p @conditions
-    # if params[:song][:name].length < 1
-    #   @song = Song.new(name: "Sorry")
-    # else
-    #   @song = Song.new(song_params)
-    # end
-    # @track = RSpotify::Track.search(@song.name).first
-    # @song.preview_url = @track.preview_url
-    # @song.artist = @track.artists.first.name
-    # respond_to do |format|
-    #   if @song.save
-    #     format.html { render "index/index" }
-    #     format.json { render json: { url: @song.preview_url , name: @song.name, artist: @song.artist } }
+    @description = @conditions['weather'].first['description']
+    @temperature = ((@conditions['main']['temp'] * 9 / 5) - 459).round(2)
+    @city = weather.city
     respond_to do |format|
       format.html { render "index/index" }
-      format.json { render json: { urls: media_urls , username: @media.first.user.username } }
+      format.json { render json: { description: @description, temperature: @temperature, city: @city } }
       end
   end
 
